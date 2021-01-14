4a7b959d8d21712cbb8b7076f8ca8d2a65beeef2
Jacob Kowalski <mr.jacob.kowalski@gmail.com>
2021-01-14 22:05:43 +0300

    Add clear focus

--- app/src/main/java/com/jwisozk/igroteka/MainActivity.kt
+++ app/src/main/java/com/jwisozk/igroteka/MainActivity.kt
@@ -2,14 +2,23 @@ package com.jwisozk.igroteka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.jwisozk.igroteka.util.KeyboardResetByClickOutside
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val keyboardResetByClickOutside = KeyboardResetByClickOutside()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        keyboardResetByClickOutside.onDispatchTouchEvent(event, currentFocus)
+        return super.dispatchTouchEvent(event)
     }
 
 }
